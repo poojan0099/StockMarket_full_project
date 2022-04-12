3fa192bc2127071fe833eaab206e38fbd5d99435
Kailas Mahavarkar <66670953+KailasMahavarkar@users.noreply.github.com>
2022-04-12 22:15:44 +0530

    Update app.py

--- app.py
+++ app.py
@@ -28,27 +28,6 @@ def livemint():
 
 @app.route('/predict')  # path set
 def predict():
-    # scaler=MinMaxScaler(feature_range=(0,1))
-    # nifty_quote = web.DataReader('^NSEI',data_source='yahoo',start='2016-01-01',end=today)
-    # #create a new dataframe
-    # new_df = nifty_quote.filter(['Close'])
-    # #get the last 60 day closing price values and convert the dataframe to an array 
-    # last_60_days = new_df[-60:].values
-    # #scale the data to be values between 0 and 1
-    # last_60_days_scaled = scaler.transform(last_60_days)
-    # #create an empty list
-    # X_test = []
-    # #APPEND THE LAST 60 DAYS
-    # X_test.append(last_60_days_scaled)
-    # #convert the X_test data set to np array
-    # X_test = np.array(X_test)
-    # #Reshape
-    # X_test = np.reshape(X_test,(X_test.shape[0],X_test.shape[1],1))
-    # #get predicted scaled price
-    # pred_price = model.predict(X_test)
-    # pred_price = scaler.inverse_transform(pred_price)
-    # print(pred_price)
-    # #nifty_quote
     return render_template('predict.html')
 
 
@@ -61,8 +40,6 @@ def sentiment():
 def chart():
     return render_template('display_candlestick.html')
 
-
-app.run(debug=True)
 
 @app.route("/status")
 def user(name):
